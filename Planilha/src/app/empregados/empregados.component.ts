01357b3c0a3b39a842739b85c54f8bedba6d52e0
Guilherme B.(gbmdevs) <guih.smi2@outlook.com>
2020-03-08 20:48:48 +0000

    Att Front

--- Planilha/src/app/empregados/empregados.component.ts
+++ Planilha/src/app/empregados/empregados.component.ts
@@ -14,10 +14,18 @@ export class EmpregadosComponent implements OnInit {
  // private REST_API_SERVIDOR = "http://172.20.0.6:8080/Planilha/"; 
   private REST_API_SERVIDOR = "http://localhost:8080/Planilha/"; 
   empregados:string[]; 
+  postData = {
+     test: 'my content',
+  };
 
-  constructor(   
-     private httpClient: HttpClient
-   )   { }
+  constructor(private httpClient: HttpClient ) {  }
+
+  clickFunction(){
+    this.httpClient.post(this.REST_API_SERVIDOR,this.postData).toPromise().then(
+      data => {
+         console.log(data);
+      });
+  }
 
   ngOnInit()  {  
     this.mandarRequisicao().subscribe((data: any[])=>{
