ea57acc88a4a530be7488c9c44a525efb5e0199e
Guilherme B.(gbmdevs) <guih.smi2@outlook.com>
2020-05-12 02:10:05 +0000

    Att 0511

--- Planilha/src/app/gastos/gastos.component.ts
+++ Planilha/src/app/gastos/gastos.component.ts
@@ -8,8 +8,8 @@ import { HttpClient } from '@angular/common/http';
 })
 export class GastosComponent implements OnInit {
    
-   private REST_API_HOST     = "http://localhost:8080/Planilha/gastos";
-   private API_HOST_DESPESAS = "http://localhost:8080/Planilha/despesasfixas"
+   private REST_API_GASTOS     = "http://localhost:8080/Planilha/gastos";
+   private REST_API_RESUMO = "http://localhost:8080/Planilha/resumo"
    
    gastos   = [];
    despesas = [];
@@ -18,12 +18,12 @@ export class GastosComponent implements OnInit {
   constructor( private httpClient : HttpClient) { }
 
   async ngOnInit(){ 
-     await this.httpClient.get(this.REST_API_HOST)
+     await this.httpClient.get(this.REST_API_GASTOS)
          .subscribe(( data: any[]) => {
              this.gastos = data;
              console.log(this.gastos); 
          }); 
-      await this.httpClient.get(this.API_HOST_DESPESAS)
+      await this.httpClient.get(this.REST_API_RESUMO)
          .subscribe(( despesas: any[]) =>{
             this.despesas = despesas;
             console.log(this.despesas);
