562850782283e6aa8f02dc873922426a808c7bb2
Guilherme B.(gbmdevs) <guih.smi2@outlook.com>
2020-05-19 22:47:09 +0000

    Att 0519

--- Planilha/src/app/cadastro-gasto/cadastro-gasto.component.ts
+++ Planilha/src/app/cadastro-gasto/cadastro-gasto.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as data from './jsonTest.json';
-
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-gasto',
@@ -10,10 +10,14 @@ import * as data from './jsonTest.json';
 export class CadastroGastoComponent implements OnInit {
 
   constructor() { }
-   
+  form: FormGroup;
   tipgastos: any = (data as any).default;
   
   ngOnInit(): void { 
+  }
+
+  onClick(){
+     console.log("Post Foi");
   }
 
 }
