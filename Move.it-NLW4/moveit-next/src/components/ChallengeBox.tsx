998049e60120d3ef961dbb3ccaf1dc2cabfbf08a
Guilherme B.(gbmdevs) <guih.smi2@outlook.com>
2021-03-03 19:37:42 +0000

    Att curso aula 03

--- Move.it-NLW4/moveit-next/src/components/ChallengeBox.tsx
+++ Move.it-NLW4/moveit-next/src/components/ChallengeBox.tsx
@@ -1,15 +1,47 @@
+import { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox(){
+    
+    const context = useContext(ChallengeContext);
+
+    const hasChallengeActive = true;
+
+    console.log(context);
+
     return(
-        <div className={styles.challengBoxContainer}>
-            <div className={styles.challengNotActive}>
-              <strong>Finalize um ciclo para receber um desafio</strong>
-              <p>
+        <div className={styles.challengeBoxContainer}>
+
+         { hasChallengeActive ? (
+             <div className={styles.challengeActive}>
+                 <header>Ganhe 400 xp</header>
+                 <main>
+                     <img src="icons/body.svg" alt="Body"/>
+                     <strong>Novo Desafio</strong>
+                     <p>Levante e faça uma caminhada de 3 minutos</p>
+                 </main>
+                 <footer>
+                     <button type='button'
+                             className={styles.challengeFailedButton}
+                     > Falhei    
+                     </button>
+                     <button type='button'
+                             className={styles.challengeSuccessedButton}
+                     > Completei
+                     </button>
+                 </footer>
+             </div>
+          ) : ( 
+             <div className={styles.challengeNotActive}>
+               <strong>Finalize um ciclo para receber um desafio</strong>
+               <p>
                    <img src="icons/level-up.svg" alt="Level up"/>
                    Avance de level completando desafios.
-              </p>
-            </div>
+               </p>
+              </div>
+          )}
+
         </div>
     )
 }
--- Move.it-NLW4/moveit-next/src/pages/_app.tsx
+++ Move.it-NLW4/moveit-next/src/pages/_app.tsx
@@ -1,8 +1,13 @@
 
 import '../styles/global.css';
 
+import { ChallengeContext } from '../contexts/ChallengeContext';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+     < ChallengeContext.Provider value={'teste'}>
+         <Component {...pageProps} />
+     </ChallengeContext.Provider> )
 }
 
 export default MyApp
