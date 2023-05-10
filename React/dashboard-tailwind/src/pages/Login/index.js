9ba8d6b8abdf04d445c303faca6da8bd6264180c
Guilherme B.(gbmdevs) <guih.smi2@outlook.com>
2023-05-10 16:55:41 +0000

    Chamando api

--- React/dashboard-tailwind/src/pages/Login/index.js
+++ React/dashboard-tailwind/src/pages/Login/index.js
@@ -1,22 +1,33 @@
 import React, { Component } from "react";
 
+import api from "../../services/api";
 import {Container, Form} from'./styles'; 
 
 import Logo from '../../assets/airbnb.svg';
 
 class Login extends Component {
     state = {
-        username: "",
+        email: "",
         senha: "",
-        error: "Não foi possivel realizar o cadastro"
+        error: ""
     };
-
-
+    
 handleSignIn = async e => {
     console.log("Envior")
     e.preventDefault();
-    const { username,senha } = this.state;
-    console.log(username + senha);
+    const { email,senha} = this.state;
+    if(!email || !senha){
+        this.setState({error: "Preenche email e senha para continuar."});
+    }else{
+        try{
+            const response = await api.post("/login",{email,senha});
+            console.log(response);
+        }catch(err){
+            console.log(err);
+            this.setState({error: "Houve um problema na autenticação"});
+        }
+    }
+    console.log(email + senha);
 }
 
 render() {
@@ -25,8 +36,8 @@ render() {
         <Form onSubmit={this.handleSignIn}>
             <img src={Logo} alt="Airbnb logo"/>
             {this.state.error && <p>{this.state.error}</p>}
-            <input type="text" placeholder="Nome de usuário" onChange={e => this.setState({username: e.target.value})}></input>
-            <input type="text" placeholder="Senha"></input>
+            <input type="text" placeholder="Nome de usuário" onChange={e => this.setState({email: e.target.value})}></input>
+            <input type="text" placeholder="Senha" onChange={e => this.setState({ senha: e.target.value})}></input>
             <button type="submit">Entrar</button>
         </Form>
     </Container>);
