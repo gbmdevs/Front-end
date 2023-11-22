77addc81f7bb336bfc8b23806641a4e3559f528d
Guilherme B.(gbmdevs) <guih.smi2@outlook.com>
2023-11-22 15:39:02 +0000

    Mexida no commit

--- React/estudo-navigate/src/App.js
+++ React/estudo-navigate/src/App.js
@@ -2,16 +2,23 @@ import './App.css';
 
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 
-
 // Paginas
-import LoginPage from './pages/Login';
 import MainPage from './pages/MainPage';
+import Login from './components/Login/Login';
+import useToken from './components/Token/useToken';
 
 const  App = () => {
+   
+  const {token, setToken} = useToken();
+  
+  if(!token){
+    return <Login setToken={setToken}/>
+  }
+
   return (
      <Router>
        <Routes>
-           <Route path="/"     element={<LoginPage/>}/>
+           <Route path="/"     element={<MainPage/>}/>
            <Route path="/dashboard" element={<MainPage/>}/>
        </Routes>     
      </Router>
